07e9d9049f3985817b2ee214f8fa213700a1e230
ahmadparvej <ahmadparvej58@gmail.com>
2022-09-06 20:32:36 +0530

    changes in db

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 
-
 const mongoose = require("mongoose")
+require("dotenv").config()
+const mongodb_url = process.env.MONGODB_URL
 
-const connection = mongoose.connect("mongodb://localhost:27017/gearbest")
+const connection = mongoose.connect(mongodb_url)
 
 module.exports = connection
